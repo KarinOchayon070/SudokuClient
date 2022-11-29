e9a107081335f36581104ade313983c66b95d67c
KarinOchayon070 <karinoch070@gmail.com>
2022-11-29 17:46:08 +0200

    init

---
+++ src/main/java/com/example/demo/HelloApplication.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 800, 580);
+        stage.setTitle("Sudoko That Solves Itself - By Karin Ochayon");
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
---
+++ src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,51 @@
+package com.example.demo;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class HelloController implements Initializable {
+    @FXML
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    public HelloController() {
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+    @FXML
+    public void onStartButtonClick(ActionEvent event) throws IOException {
+
+
+        Parent root = (Parent) FXMLLoader.load(getClass().getResource("grid.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+//        Scene scene = new Scene(root);
+//        stage.setTitle("Easy");
+//        stage.setScene(scene);
+//        stage.initModality(Modality.APPLICATION_MODAL);
+//        stage.show();
+    }
+
+
+}
